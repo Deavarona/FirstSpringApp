21f786b8dfd79236491dca4d5ba442b413c3ff27
kkotto <ksen.sharupich.work@gmail.com>
2023-02-07 13:00:11 +0300

    Lesson 2, 7-2-2023

---
+++ src/main/java/org/kkotto/TestSpring.java
@@ -0,0 +1,14 @@
+package org.kkotto;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "application.xml"
+        );
+        TestBean testBean = context.getBean("testBean", TestBean.class);
+        System.out.println(testBean.getName());
+        context.close();
+    }
+}
