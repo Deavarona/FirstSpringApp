0f47bcf0a7446787ed68d723f57fb9fc76178b68
kkotto <ksen.sharupich.work@gmail.com>
2023-02-09 21:37:40 +0300

    Lesson 6: DI

--- src/main/java/org/kkotto/MusicPlayer.java
+++ src/main/java/org/kkotto/MusicPlayer.java
@@ -1,13 +1,35 @@
 package org.kkotto;
 
 public class MusicPlayer {
-    public Music music;
-
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
+    private Music music;
+    private String name;
+    private int volume;
 
     public void playMusic() {
         System.out.println("Playing " + music.getSong());
+    }
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
+    public Music getMusic() {
+        return music;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
     }
 }
