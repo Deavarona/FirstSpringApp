3df70fc015d17aa044eb3d10e1c942e713652c15
kkotto <ksen.sharupich.work@gmail.com>
2023-02-19 00:34:32 +0300

    Lesson 13

--- src/main/java/org/kkotto/MusicPlayer.java
+++ src/main/java/org/kkotto/MusicPlayer.java
@@ -4,15 +4,25 @@ import org.kkotto.music.Music;
 import org.kkotto.music.genres.Genre;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.util.Random;
 
 @Component
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class MusicPlayer {
     private Music classicalPlaylist;
     private Music rockPlaylist;
     private Music rapPlaylist;
+    @Value("${musicPlayer.volume}")
+    private int volume;
+    @Value("${musicPlayer.name}")
+    private String name;
 
     @Autowired
     public MusicPlayer(@Qualifier("classicalMusic") Music classicalPlaylist,
@@ -39,5 +49,23 @@ public class MusicPlayer {
         Random fate = new Random();
         int songNumber = fate.nextInt(currentPlaylist.getNumberOfSongs());
         return currentPlaylist.getSong(songNumber);
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @PostConstruct
+    private void init() {
+        System.out.println("Player opened");
+    }
+
+    @PreDestroy
+    private void destroy() {
+        System.out.println("Player closed");
     }
 }
--- src/main/java/org/kkotto/music/genres/RapMusic.java
+++ src/main/java/org/kkotto/music/genres/RapMusic.java
@@ -1,11 +1,16 @@
 package org.kkotto.music.genres;
 
 import org.kkotto.music.Music;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.util.List;
 
 @Component
+@Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class RapMusic implements Music {
     private List<String> playlist = List.of("Monster", "Madness", "Undead");
 
@@ -17,5 +22,15 @@ public class RapMusic implements Music {
     @Override
     public String getSong(int songNumber) {
         return playlist.get(songNumber);
+    }
+
+    @PostConstruct
+    private void init() {
+        System.out.println("Rap started");
+    }
+
+    @PreDestroy
+    private void destroy() {
+        System.out.println("Rap finished");
     }
 }
